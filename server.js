408573807d605f5609d870c01322059b7ae7619f
Ishan Bankhele <bankheleishan@gmail.com>
2023-11-18 18:51:50 +0530

    cors stuff again

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://game-scape-community-website-mern-project.vercel.app/',
     credentials: true,
 }))
 
